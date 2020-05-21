2222154648508b77e66a06f16b05e1f4e3fd5baa
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-05-21 14:30:09 +0300

    Modifies scripts.js uncomments addNumber function call

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ let convertTemperature = (convertTo,temperatureValue) =>{
 	if(convertTo === "C"){
 		convertedTemperature=temperatureValue+" converted into Celsius is: "+((temperatureValue - 32)/1.8).toFixed(2);
 	}else if(convertTo === "F"){
-		convertedTemperature=temperatureValue+" converted into Celsius is: "+((temperatureValue * 1.8) + 32).toFixed(2);
+		convertedTemperature=temperatureValue+" converted into Fahrenheit is: "+((temperatureValue * 1.8) + 32).toFixed(2);
 	}else{
 		convertedTemperature="BAD METRIC";
 	}
@@ -19,18 +19,18 @@ let convertTemperature = (convertTo,temperatureValue) =>{
 /******************
 	Adding Two Numbers
 **********************/
-// number1=parseInt(prompt("Enter a number: "));
-// number2=parseInt(prompt("Enter another number: "));
-// totalNumberSum=addNumbers(number1,number2);
-// alert(totalNumberSum);
+number1=parseInt(prompt("Enter a number: "));
+number2=parseInt(prompt("Enter another number: "));
+totalNumberSum=addNumbers(number1,number2);
+alert(totalNumberSum);
 /********************
 	Convert Temperature
 ***********************/
-metric=prompt("Please enter the conversion metric.\n C:\t Celsius \n F:\t Fahrenheit");
-tempValue=parseFloat(prompt("Please enter the temperature value to convert"));
+metric=prompt("Please enter the conversion metric.\n\n C:\t Celsius \n\n F:\t Fahrenheit\n\n");
+tempValue=parseFloat(prompt("Please enter the temperature value\n\n to convert (Digits Only)\n\n"));
 tempConverted=convertTemperature(metric.toUpperCase(),tempValue);
 if(tempConverted === "BAD METRIC"){
-	tempOutputMessage= "Operation Failed. Invalid conversion metric provided.\nEnter C for celsius and F for Fahrenheit";
+	tempOutputMessage= "\tOPERATION FAILED. \n\nInvalid conversion metric provided.\n\nEnter C for celsius and F for Fahrenheit\n\n";
 }else{
 	tempOutputMessage= tempConverted;
 }
