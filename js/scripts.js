1ee518e8a4319e4ffdab3a86f354c2cca373f8ee
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-06-12 10:23:51 +0300

    Formats the calculation outputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,53 @@
+let numberWithCommas= (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 let addNumbers = (firstNumber, secondNumber) => {
-	return firstNumber + secondNumber;
+	return numberWithCommas((firstNumber + secondNumber).toFixed(2));
 }
 
 let subtractNumbers = (firstNumber, secondNumber) => {
-	return firstNumber - secondNumber;
+	return numberWithCommas((firstNumber - secondNumber).toFixed(2));
 }
 
 let multiplyNumbers = (firstNumber, secondNumber) => {
-	return firstNumber * secondNumber;
+	return numberWithCommas((firstNumber * secondNumber).toFixed(2));
 }
 
 let divideNumbers = (firstNumber, secondNumber) => {
 	if(secondNumber === 0){
 		return 0;
 	}else{
-		return firstNumber/secondNumber;
+		return numberWithCommas((firstNumber/secondNumber).toFixed(2));
 	}
 }
 //User Interface Logic
 $(document).ready(() => {
 	$("#add").submit((event)=> {
 		event.preventDefault();
-		let firstNumber=parseInt($("#add1").val());
-		let secondNumber=parseInt($("#add2").val());
+		let firstNumber=parseFloat($("#add1").val());
+		let secondNumber=parseFloat($("#add2").val());
 		$("#add-output").text(addNumbers(firstNumber,secondNumber)).fadeIn();
 	});
 
 	$("#subtract").submit((event)=> {
 		event.preventDefault();
-		let firstNumber=parseInt($("#subtract1").val());
-		let secondNumber=parseInt($("#subtract2").val());
+		let firstNumber=parseFloat($("#subtract1").val());
+		let secondNumber=parseFloat($("#subtract2").val());
 		$("#subtract-output").text(subtractNumbers(firstNumber,secondNumber)).fadeIn();
 	});
 
 	$("#multiply").submit((event)=> {
 		event.preventDefault();
-		let firstNumber=parseInt($("#multiply1").val());
-		let secondNumber=parseInt($("#multiply2").val());
+		let firstNumber=parseFloat($("#multiply1").val());
+		let secondNumber=parseFloat($("#multiply2").val());
 		$("#multiply-output").text(multiplyNumbers(firstNumber,secondNumber)).fadeIn();
 	});
 
 	$("#divide").submit((event)=> {
 		event.preventDefault();
-		let firstNumber=parseInt($("#divide1").val());
-		let secondNumber=parseInt($("#divide2").val());
+		let firstNumber=parseFloat($("#divide1").val());
+		let secondNumber=parseFloat($("#divide2").val());
 		$("#divide-output").text(divideNumbers(firstNumber,secondNumber)).fadeIn();
 	});
 });
